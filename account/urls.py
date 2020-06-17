1e74fc1d4aef8e7d629955a3a28056c1aefba216
170 <170@gmail.com>
2020-06-17 23:28:35 +0300

    Форма входа, без привязки

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .forms import UserLoginForm
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(authentication_form=UserLoginForm), name='login'),
+]
